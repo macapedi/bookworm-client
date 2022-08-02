e287462ce72f34ebe8452998450b211c73362924
macapedi <macapedi90@gmail.com>
2022-08-01 22:25:22 -0400

    basic components and pages, files and folders

---
+++ src/pages/BookDetailsPage/BookDetailsPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./BookDetailsPage.scss";
+
+class BookDetailsPage extends React.Component {
+
+
+
+render () {
+
+    return (
+      <>
+      <h1>This is Book Details Page</h1>
+     
+      </>
+    );
+  }
+}
+  
+  export default BookDetailsPage;
+  
---
+++ src/pages/UserPage/UserPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./UserPage.scss";
+
+class UserPage extends React.Component {
+
+
+
+render () {
+
+    return (
+      <>
+      <h1>This is User Page</h1>
+     
+      </>
+    );
+  }
+}
+  
+  export default UserPage;
