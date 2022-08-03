87200109f40796697663e4a4259d64ade7333de9
macapedi <macapedi90@gmail.com>
2022-08-03 19:49:26 -0400

    api call and books received in books page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.scss';
+import React from 'react';
+import axios from 'axios';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/Header/Header";
 import HomePage from './pages/HomePage/HomePage';
@@ -9,7 +11,34 @@ import UserPage from "./pages/UserPage/UserPage";
 import Login from './pages/Login/Login';
 
 
-function App() {
+
+class App extends React.Component {
+
+  state = {
+    booksList: null
+
+  }
+
+  async componentDidMount() {
+
+    const url = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json'
+    const key = "?api-key=MPpcrAgZYL3NCtOTzOVpM9K9D4DJGWee"
+    const response = await axios.get(`${url}${key}`)
+    const booksList = response.data;
+    console.log(response.data);
+
+
+    this.setState({
+      booksList
+    })
+  }
+
+
+render() {
+
+if(this.state.booksList){
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -19,19 +48,21 @@ function App() {
             <Redirect to="/login" />
           </Route>
           <Route path="/login" exact component={Login} />
-          <Route path="/books" exact component={HomePage} />
+          <Route path="/books" exact >
+            <HomePage booksList={this.state.booksList}/>
+          </Route>
           <Route path="/books/:id" component={BookDetailsPage} />
           <Route path="/users" exact component={PublicShelvesPage} />
           <Route path="/users/:id" component={UserPage} />
         </Switch>
       </BrowserRouter>
 
-
-
-
-
     </div>
   );
+} else{
+  <p>Loading...</p>
+}
+}
 }
 
 export default App;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import "./HomePage.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
 
+  state = {
+    booksList: this.props.booksList
+
+  }
 
 
-render () {
 
-    return (
-      <>
-      <h1>This is Home Page</h1>
-     
-      </>
-    );
+
+
+
+  render() {
+    console.log(this.state.booksList.results.books);
+
+    if (this.state.booksList) {
+
+      return (
+        <>
+          <h1>This is Home Page</h1>
+          <ul className='books__image-container'>
+
+            {this.state.booksList.results.books.map((book, index) => {
+              console.log(book.primary_isbn10);
+
+              return (
+
+
+                <li key={book.primary_isbn10} className="books__container">
+                  <Link to={`/books/${book.primary_isbn10}`} className='books__container'>
+
+                    <img className='books__image' src={book.book_image}></img>
+
+
+                  </Link>
+                </li>
+              )
+
+            })}
+
+          </ul>
+
+        </>
+      );
+    } else {
+      return <p>Loading...</p>
+    }
   }
 }
-  
-  export default HomePage;
-  
+
+export default HomePage;
