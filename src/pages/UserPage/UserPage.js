af0306ee9fbea9903baac082362b5a46d15d9d1a
macapedi <macapedi90@gmail.com>
2022-08-08 09:51:19 -0400

    badges added, styling for user Page

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import "./UserPage.scss";
 import axios from "axios"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import megaWorm from "../../assets/images/megaWorm.png";
+import superWorm from "../../assets/images/superWorm.png";
+import babyWorm from "../../assets/images/babyWorm.png"
 
 
 class UserPage extends React.Component {
 
   state = {
-    userBookList: []
+    userBookList: [],
+    readingList: [],
+    wishList: [],
+    readList: [],
+    droppedList: []
   }
 
 
@@ -17,9 +24,24 @@ class UserPage extends React.Component {
     const userBookListReq = await axios.get(`http://localhost:8080/users/${userId}`);
 
     const userBookList = userBookListReq.data.inventoryBooks;
-   
+    console.log(userBookList);
+
+    const finishedList = userBookList.filter((book) => book.status === "Finished");
+    const wishList = userBookList.filter((book) => book.status === "Wish List");
+    const readingList = userBookList.filter((book) => book.status === "Reading");
+    const droppedList = userBookList.filter((book) => book.status === "Dropped");
+
+    console.log(finishedList);
+
+
+
+
     this.setState({
-      userBookList
+      userBookList,
+      finishedList,
+      wishList,
+      readingList,
+      droppedList
     })
 
   }
@@ -30,64 +52,189 @@ class UserPage extends React.Component {
     const userBookListReq = await axios.get(`http://localhost:8080/users/${userId}`);
 
     const userBookList = userBookListReq.data.inventoryBooks;
-   
+    console.log(userBookList);
+
+    const finishedList = userBookList.filter((book) => book.status === "Finished");
+    const wishList = userBookList.filter((book) => book.status === "Wish List");
+    const readingList = userBookList.filter((book) => book.status === "Reading");
+    const droppedList = userBookList.filter((book) => book.status === "Dropped");
+
+    console.log(finishedList);
+
+
+
+
     this.setState({
-      userBookList
+      userBookList,
+      finishedList,
+      wishList,
+      readingList,
+      droppedList
     })
 
   }
 
-  
+
 
   render() {
-
+    const userId = this.props.routerProps.match.params.id;
+    const users = this.props.usersList;
 
     if (this.props.usersList && this.state.userBookList) {
 
-      const userId = this.props.routerProps.match.params.id;
-      const users = this.props.usersList;
+
 
 
       const singleUser = users.filter((user) => userId === user.id);
       const { name } = singleUser[0];
 
       return (
-        <>
-          <h1 className='user__title'>Single User Page</h1>
-          <p>{name}</p>
+        <main className='user'>
 
-          <ul className='books__image-container'>
-       
+          <h1 className='user__title'>{name}'s Shelves</h1>
+          {this.state.finishedList &&
+            <div className='user__badge'>
+              <p>Total of {this.state.finishedList.length} books finished</p>
+              <img src={babyWorm} alt="baby worm bage" className='badge'></img>
 
-            {this.state.userBookList.map((book, index) => {
+            </div>
+          }
 
+          {this.state.readingList &&
+            <>
+              <div className='user__list-wrapper'>
+                <h3 className='user__subtitle'>Reading</h3>
 
-              return (
-
-
-                <li key={book.primary_isbn10} className="books__container">
-                  <Link to={`/users/${userId}/${book.primary_isbn10}`} className='books__container'>
-
-                    <div className='books__wrapper'>
-                      {/* <p className='books__rank'> {book.rank}</p> */}
-                      <img className='books__image' src={book.book_image}></img>
-                    </div>
-
-                  </Link>
-                </li>
-              )
-
-            })}
-
-          </ul>
+                <ul className='books__image-container'>
 
 
+                  {this.state.readingList.map((book, index) => {
+
+                    return (
 
 
+                      <li key={book.primary_isbn10} className="books__container">
+                        <Link to={`/users/${userId}/${book.primary_isbn10}`} className='books__container'>
 
-        </>
+                          <div className='books__wrapper'>
+                            {/* <p className='books__rank'> {book.rank}</p> */}
+                            <img className='books__image' src={book.book_image}></img>
+                          </div>
+
+                        </Link>
+                      </li>
+                    )
+
+                  })}
+
+                </ul>
+              </div>
+            </>
+          }
+
+          {this.state.wishList &&
+            <>
+              <div className='user__list-wrapper'>
+                <div className='user__subtitle-wrapper'>
+                  <h3 className='user__subtitle'>Wish List</h3>
+                </div>
+
+                <ul className='books__image-container'>
+
+
+                  {this.state.wishList.map((book, index) => {
+
+                    return (
+
+
+                      <li key={book.primary_isbn10} className="books__container">
+                        <Link to={`/users/${userId}/${book.primary_isbn10}`} className='books__container'>
+
+                          <div className='books__wrapper'>
+                            {/* <p className='books__rank'> {book.rank}</p> */}
+                            <img className='books__image' src={book.book_image}></img>
+                          </div>
+
+                        </Link>
+                      </li>
+                    )
+
+                  })}
+
+                </ul>
+              </div>
+            </>
+          }
+
+          {this.state.finishedList &&
+            <>
+              <div className='user__list-wrapper'>
+                <div className='user__subtitle-wrapper'>
+                  <h3 className='user__subtitle'>Finished</h3>
+                </div>
+
+                <ul className='books__image-container'>
+
+
+                  {this.state.finishedList.map((book, index) => {
+
+                    return (
+
+
+                      <li key={book.primary_isbn10} className="books__container">
+                        <Link to={`/users/${userId}/${book.primary_isbn10}`} className='books__container'>
+
+                          <div className='books__wrapper'>
+                            {/* <p className='books__rank'> {book.rank}</p> */}
+                            <img className='books__image' src={book.book_image}></img>
+                          </div>
+
+                        </Link>
+                      </li>
+                    )
+
+                  })}
+
+                </ul>
+              </div>
+            </>
+          }
+
+          {this.state.droppedList &&
+            <>
+              <div className='user__list-wrapper'>
+                <h3 className='user__subtitle'>Dropped</h3>
+
+                <ul className='books__image-container'>
+
+
+                  {this.state.droppedList.map((book, index) => {
+
+                    return (
+
+
+                      <li key={book.primary_isbn10} className="books__container">
+                        <Link to={`/users/${userId}/${book.primary_isbn10}`} className='books__container'>
+
+                          <div className='books__wrapper'>
+                            {/* <p className='books__rank'> {book.rank}</p> */}
+                            <img className='books__image' src={book.book_image}></img>
+                          </div>
+
+                        </Link>
+                      </li>
+                    )
+
+                  })}
+
+                </ul>
+              </div>
+            </>
+          }
+        </main>
       )
     }
+
   }
 }
 
